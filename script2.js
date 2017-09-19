450e759985cef60b61975b2627df250ce071703e
Marina <marina_kul@tut.by>
2017-09-19 17:41:12 +0300

    task is done

---
+++ script2.js
@@ -0,0 +1,14 @@
+let podarok = {
+    name: 'New_Years_present'
+};
+
+let myset = object(podarok);
+
+
+function object(item){
+function MyFunction(){};
+
+MyFunction.prototype = item;
+return new MyFunction();
+}
+console.log(myset.name);
